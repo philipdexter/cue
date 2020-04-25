730091d353b55d88a27583a18a4401e9738e9625
Philip Dexter <philip.dexter@gmail.com>
2020-04-25 16:57:39 +0300

    Remove save, history, and restore

--- cmd/cue/cmd/repl.go
+++ cmd/cue/cmd/repl.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"github.com/chzyer/readline"
@@ -81,18 +80,13 @@ func runRepl(cmd *Command, args []string) error {
 		}
 
 		if strings.HasPrefix(text, ":") {
-			err := ExecCommand(text)
+			err := execCommand(text)
 			if err != nil {
 				fmt.Println(err)
 			}
 		} else {
-			err := AddExpr(text)
+			err := addExpr(text)
 			if err != nil {
-				fmt.Println(err)
-			}
-			err = CheckpointHistory()
-			if err != nil {
-				// TODO undo AddExpr and CheckpointHistory
 				fmt.Println(err)
 			}
 		}
@@ -107,12 +101,11 @@ func runRepl(cmd *Command, args []string) error {
 
 var r cue.Runtime
 
-var history []cue.Value // Maybe store the built instance if build returns a new one every time
 var bi *build.Instance
 
 var inModule = false
 
-func AddExpr(expr string) error {
+func addExpr(expr string) error {
 	// TODO maybe try to build here, so we can
 	// catch errors earlier
 
@@ -139,16 +132,7 @@ func AddExpr(expr string) error {
 	}
 }
 
-func CheckpointHistory() error {
-	bii, err := BuildI()
-	if err != nil {
-		return err
-	}
-	history = append(history, bii.Value())
-	return nil
-}
-
-func BuildI() (*cue.Instance, error) {
+func buildI() (*cue.Instance, error) {
 	return r.Build(bi)
 }
 
@@ -186,17 +170,9 @@ func init() {
 	inModule = !os.IsNotExist(err)
 
 	resetI()
-
-	bii, err := BuildI()
-	if err != nil {
-		panic(err)
-	}
-
-	history = make([]cue.Value, 0)
-	history = append(history, bii.Value())
 }
 
-func ExecCommand(text string) error {
+func execCommand(text string) error {
 	commandParts := strings.Fields(strings.TrimPrefix(text, ":"))
 	command := commandParts[0]
 	if command == "i" || command == "inject" {
@@ -205,63 +181,14 @@ func ExecCommand(text string) error {
 		if err != nil {
 			return err
 		}
-		return CheckpointHistory()
+		return nil
 	} else if command == "help" {
+		// TODO update help
 		fmt.Println("help -- print help text")
 		fmt.Println("l/lookup <path> -- print the value at <path>")
-		fmt.Println("h/history -- print history indexed by histnums")
 		fmt.Println("p/print -- print the current value")
-		fmt.Println("r/restore <histnum> -- restore the value at <histnum>")
-		fmt.Println("s/save <where> -- save the current value into <where>")
-	} else if command == "s" || command == "save" {
-		i, err := r.Compile("repl", ``)
-		if err != nil {
-			return err
-		}
-		newv := i.Value()
-		newv = newv.Fill(history[len(history)-1], strings.Split(commandParts[1], ".")...)
-		if newv.Err() != nil {
-			return newv.Err()
-		}
-		resetI()
-		bytes, err := format.Node(newv.Syntax())
-		if err != nil {
-			return err
-		}
-		err = AddExpr(string(bytes))
-		if err != nil {
-			return err
-		}
-		return CheckpointHistory()
-	} else if command == "r" || command == "restore" {
-		i, err := strconv.ParseInt(commandParts[1], 10, 64)
-		if err != nil {
-			return err
-		}
-		resetI()
-		bytes, err := format.Node(history[i].Syntax())
-		if err != nil {
-			return err
-		}
-		err = AddExpr(string(bytes))
-		if err != nil {
-			return err
-		}
-	} else if command == "h" || command == "history" {
-		fmt.Printf("\033[34m====\033[0m\n")
-		for i, vv := range history {
-			fmt.Printf("\033[34m%d\033[0m\n", i)
-			err := pprint(vv)
-			if err != nil {
-				return err
-			}
-			if i != len(history)-1 {
-				fmt.Printf("\033[34m----\033[0m\n")
-			}
-		}
-		fmt.Printf("\033[34m====\033[0m\n")
 	} else if command == "p" || command == "print" {
-		bii, err := BuildI()
+		bii, err := buildI()
 		if err != nil {
 			return err
 		}
@@ -270,7 +197,7 @@ func ExecCommand(text string) error {
 			return err
 		}
 	} else if command == "l" || command == "lookup" {
-		bii, err := BuildI()
+		bii, err := buildI()
 		if err != nil {
 			return err
 		}
